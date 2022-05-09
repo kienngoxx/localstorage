0e9183c1db64e9bd478ce86507886a786d02fcb5
Ngô Văn Kiên <105222232+kienngoxx@users.noreply.github.com>
2022-05-09 23:33:16 +0700

    Add files via upload

---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Credential.java
@@ -0,0 +1,7 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+public class Credential {
+
+
+	
+}
---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/User.java
@@ -0,0 +1,18 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+import java.util.List;
+
+public class User {
+	
+	private String userId;
+	
+	private String userName;
+	
+	private String password;
+	
+	private List<File> fileList;
+	
+	private List<Note> noteList;
+	
+	private List<Credential> credentialList;
+}
